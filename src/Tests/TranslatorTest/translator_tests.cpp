6b8dc26d0d0cb62e0ee0529be7a67792ee4a6f1e
Andrii Dovbush <dovbush.andrii@gmail.com>
2021-03-26 20:21:57 +0200

    Translator::StrWithBaseToNum tests were added

---
+++ src/Tests/TranslatorTest/translator_tests.cpp
@@ -0,0 +1,45 @@
+//======== Copyright (c) 2021, FIT VUT Brno, All rights reserved. ============//
+//
+// Purpose:     Test-Driven-Development 
+//
+// $NoKeywords: $ivs_project_2 $translator_tests.cpp
+// $Author:     
+// $Date:       $2021-03-26
+//============================================================================//
+
+#include "gtest/gtest.h"
+#include "controller.h"
+
+class TranslatorTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp() {
+    }
+};
+
+/*
+* @Author - Andrii Dovbush, xdovbu00
+*/
+TEST_F(TranslatorTest, StrToDouble) {
+    EXPECT_EQ(Translator::StrWithBaseToNum("101", 2), 5);
+    EXPECT_EQ(Translator::StrWithBaseToNum("101", 3), 10);
+    EXPECT_EQ(Translator::StrWithBaseToNum("101", 4), 17);
+
+    EXPECT_EQ(Translator::StrWithBaseToNum("-101", 2), -5);
+
+    EXPECT_ANY_THROW(Translator::StrWithBaseToNum("121", 2));
+    EXPECT_ANY_THROW(Translator::StrWithBaseToNum("1B1", 8));
+
+    EXPECT_EQ(Translator::StrWithBaseToNum("1A", 11), 21);
+    EXPECT_EQ(Translator::StrWithBaseToNum("3F", 16), 63);
+
+    EXPECT_EQ(Translator::StrWithBaseToNum("-1A", 11), -21);
+    EXPECT_EQ(Translator::StrWithBaseToNum("--1A", 11), 21);
+    EXPECT_EQ(Translator::StrWithBaseToNum("---1A", 11), -21);
+
+    EXPECT_ANY_THROW(Translator::StrWithBaseToNum("0", 1));
+    EXPECT_ANY_THROW(Translator::StrWithBaseToNum("1B1", 17));
+
+    //TODO: FRACTION TESTS
+    EXPECT_EQ(Translator::StrWithBaseToNum("10.01", 2), 2.25);
+}
