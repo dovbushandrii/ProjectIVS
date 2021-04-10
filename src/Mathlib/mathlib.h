7760606f5e958e757a402ea5473398388f4a7305
Andrii Dovbush <dovbush.andrii@gmail.com>
2021-04-10 11:36:34 +0300

    Add,Sub,Power,Lg,Ln functions were implemented. Pow func was renamed to Power

--- src/Mathlib/mathlib.h
+++ src/Mathlib/mathlib.h
@@ -1,106 +1,162 @@
 #include <math.h>
+#include <stdexcept>
 #include <string>
-
-using namespace std;
-
-class Equation {
-public:
-	//TODO description, function
-	/**
-	*  Solves equation 
-	* @brief
-	* @author
-	* @param equation
-	* @returns
-	*/
-	static double solveEquation(string equation);
-};
 
 class Function {
 public:
-	//TODO description, function
-	/**
-	*  a + b
-	* @brief
-	* @author
-	* @param a
-	* @param b
-	* @returns
-	*/
-	static double add(const double a, const double b);
+    /**
+    * Variable to set precision of calculations
+    * in Function::lg and Function::ln
+    * functions.
+    * Should be initialized at the start
+    * of the programm
+    */
+    static double precision;
 
-	//TODO description, function
-	/**
-	*  a - b
-	* @brief
-	* @author
-	* @param a
-	* @param b
-	* @returns
-	*/
-	static double sub(const double a, const double b);
+    /**
+    * @brief Sum function ( a + b )
+    * @author Andrii Dovbush xdovbu00
+    * @param a addend
+    * @param b addend
+    * @returns sum of a and b
+    */
+    static double add(const double a, const double b) {
+        return a + b;
+    }
 
-	//TODO description, function
-	/**
-	*  a * b
-	* @brief
-	* @author
-	* @param a
-	* @param b
-	* @returns
-	*/
-	static double mul(const double a, const double b);
+    /**
+    * @brief Subtraction function ( a - b )
+    * @author Andrii Dovbush xdovbu00
+    * @param a minuend
+    * @param b subtrahend
+    * @returns difference
+    */
+    static double sub(const double a, const double b) {
+        return a - b;
+    }
 
-	/**
-	* @brief Division function ( a / b )
-	* @author Andrii Dovbush xdovbu00
-	* @param a divided
-	* @param b divider
-	* @returns division a by b
-	*/
+    /**
+    * @brief
+    * @author
+    * @param a
+    * @param b
+    * @returns
+    */
+    static double mul(const double a, const double b) {
+        return 0;
+    }
+
+    /**
+    * @brief Division function ( a / b )
+    * @author Andrii Dovbush xdovbu00
+    * @param a divided
+    * @param b divider
+    * @returns division a by b
+    */
     static double div(const double a, const double b) {
         return a / b;
     }
 
-	//TODO description, function
-	/**
-	*  a^b
-	* @brief
-	* @author
-	* @param a
-	* @param b
-	* @returns
-	*/
-	static double pow(const double a, const double b);
+    /**
+    * @brief Power function ( a^b )
+    * @author Andrii Dovbush xdovbu00
+    * @param a base value
+    * @param b power
+    * @returns division a by b
+    */
+    static double power(const double a, const double b) {
+        return pow(a, b);
+    }
 
-	//TODO description, function
-	/**
-	*  a^(1/b)
-	* @brief
-	* @author
-	* @param a
-	* @param b
-	* @returns
-	*/
-	static double root(const double a, const double b);
+    //TODO description, function
+    /**
+    *  a^(1/b)
+    * @brief
+    * @author
+    * @param a
+    * @param b
+    * @returns
+    */
+    static double root(const double a, const double b) {
+        return 0;
+    }
 
-	//TODO description, function
-	/**
-	*  lg(a)
-	* @brief
-	* @author
-	* @param a
-	* @returns
-	*/
-	static double lg(const double a);
+    /**
+    * @brief Common logarithm ( lg(a) )
+    * @author Andrii Dovbush xdovbu00
+    * @param a argument that should be greater then 0
+    * @returns power
+    */
+    static double lg(const double a) {
+        double result = -1;
+        if (a <= 0) {
+            throw std::invalid_argument("mathlib::Function.lg: Argument must be greater then zero");
+        }
+        else {
+            double newValue = 0;
+            double aCopy = a;
+            if (a > 1) {
+                while (aCopy >= 10) {
+                    aCopy = aCopy / 10;
+                    newValue++;
+                }
+            }
+            else {
+                while (aCopy < 1) {
+                    aCopy = aCopy * 10;
+                    newValue--;
+                }
+            }
 
-	//TODO description, function
-	/**
-	*  ln(a)
-	* @brief
-	* @author
-	* @param a
-	* @returns
-	*/
-	static double ln(const double a);
+            while (abs(newValue - result) > precision) {
+                result = newValue;
+                double almostA = pow(10, newValue);
+                newValue += (a - almostA) / (a * 10);
+            }
+        }
+        return result;
+    }
+
+    /**
+    * @brief Natural logarithm ( ln(a) )
+    * @author Andrii Dovbush xdovbu00
+    * @param a argument that should be greater then 0 and less then 2
+    * @returns power
+    */
+    static double ln(const double a) {
+        double result = 0;
+        if (a <= 0) {
+            throw std::invalid_argument("mathlib::Function.ln: Argument must be greater then zero");
+        }
+        else {
+            double x = a - 1;
+            double newValue = x;
+            double addend = x;
+            for (int i = 2; abs(newValue - result) > precision; i++) {
+                result = newValue;
+                addend = addend * x * (-1);
+                newValue = newValue + addend / i;
+            }
+            result = newValue;
+        }
+        return result;
+    }
+};
+
+//Static Variable Initialization
+double Function::precision = 0.000000001;
+
+class Equation {
+public:
+    //TODO description, function
+    /**
+    *  Solves equation
+    * @brief
+    * @author
+    * @param equation
+    * @returns
+    */
+    static double solveEquation(std::string equation) {
+        return 0;
+    }
 };
