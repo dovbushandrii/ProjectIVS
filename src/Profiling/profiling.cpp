7595c5241f1f0df24a0147f6d8bc6a8aa23f9668
Andrii Dovbush <dovbush.andrii@gmail.com>
2021-04-29 17:37:06 +0200

    Profiling implemented

--- src/Profiling/profiling.cpp
+++ src/Profiling/profiling.cpp
@@ -15,10 +15,34 @@
 */
 //============================================================================//
 
-#include<iostream>
+#include "../Mathlib/mathlib.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-	cout << "Sample profiling source code" << endl;
-	return 0;
+    vector<double> value;
+    double number = 0, result = 0, summ = 0;
+    while (cin >> number) {
+        value.push_back(number);
+        summ = Function::add(summ, number);
+    }
+
+    if (value.size()) {
+        double mean = Function::div(summ, (double)value.size());
+        summ = 0;
+
+        for (size_t i = 0; i < value.size(); i++) {
+            double squareOfDiff = Function::power(Function::sub(value[i], mean), 2.0);
+            summ = Function::add(summ, squareOfDiff);
+        }
+
+        result = Function::div(summ, (double)(value.size() - 1));
+        result = Function::sqrt(result);
+    }
+
+    cout << "Standard deviation(For " << value.size() << " values): " << result;
+    return 0;
 }
 
